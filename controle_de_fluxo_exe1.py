81dca7c0b51e7049363bf6d462175237be79eeef
Driiscoll <italoba.leite@gmail.com>
2021-10-19 20:54:02 -0300

    adicionando controle de fluxo exercicio 1

---
+++ controle_de_fluxo_exe1.py
@@ -0,0 +1,13 @@
+ano_nasc = int(input("Digite o ano do seu nascimento: "))
+
+if ano_nasc <= 1964:
+    print("Baby Boomer")
+
+elif ano_nasc >= 1965 and ano_nasc <=1979:
+    print("Geração: X")
+
+elif ano_nasc >= 1980 and ano_nasc <= 1994:
+    print("Geração: Y")
+
+elif ano_nasc >= 1995:
+    print("Geração: Z")
