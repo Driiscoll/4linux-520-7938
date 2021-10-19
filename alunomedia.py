aebb67b9eb2a2830d6380bc8f41f7346e50a0b03
Driiscoll <italoba.leite@gmail.com>
2021-10-19 20:01:18 -0300

    alterando media de aluno

--- alunomedia.py
+++ alunomedia.py
@@ -1,14 +1,14 @@
-nota1 = int(input("digite a primeira nota"))
-nota2 = int(input("digite a segunda nota"))
-nota3 = int(input("digite a terceira nota"))
+nota1 = float(input("digite a primeira nota"))
+nota2 = float(input("digite a segunda nota"))
+nota3 = float(input("digite a terceira nota"))
 
 media = (nota1 + nota2 + nota3)/3
 
-if media >= 6:
+if media > 6:
     print("Aluno aprovado")
 
 elif media >= 4:
     print("aluno ficará em recuperação")
 
-elif media < 4 and media > 0:
+elif media < 4:
     print("aluno reprovado")
