42294676e8916344c5be53079525b7182ab500a7
Driiscoll <italoba.leite@gmail.com>
2021-10-19 19:51:11 -0300

    alterando ifelifelse

--- ifelifelse.py
+++ ifelifelse.py
@@ -1,7 +1,13 @@
-habilitado = input("Você é habilitado? ").lower()
+habilitado = input("Você é habilitado? ").lower().strip()
 
 if habilitado.lower().startswith('s') == 'sim':
     print("Você pode dirigir!")
 
 elif habilitado.lower().startswith('n'):
     print("Você não deve dirifigir")
+
+elif habilitado.find('s') != -1:
+    print("Acho que você pode dirigir...")
+
+else:
+    print("não entendi")
