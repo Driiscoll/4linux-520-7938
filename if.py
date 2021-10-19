6e418677b543d3979533fe838eb45181df54ccf0
Driiscoll <italoba.leite@gmail.com>
2021-10-19 19:38:05 -0300

    if e ifelse

---
+++ if.py
@@ -0,0 +1,12 @@
+print("olá! Seja bem vindo!")
+
+nome = input("qual é o seu nome? ")
+idade = int(input("Quantos anos você tem? "))
+
+print(f"Boa noire , {nome}!")
+
+if idade >=18:
+    print("você já é maior de idade! Pode dirigir e beber!")
+    print("mas não junto!")
+
+print("Até logo!")
---
+++ ifelse.py
@@ -0,0 +1,7 @@
+habilitado = input("Você é habilitado? ")
+
+if habilitado.lower() == 'sim':
+    print("Você pode dirigir!")
+
+else:
+    print("Você não deve dirifigir")
